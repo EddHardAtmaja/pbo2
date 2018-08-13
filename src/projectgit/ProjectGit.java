d12d12e1d87465637541b6cb162bd01a53f872d9
Edd <Edd@Edd-PC>
2018-08-13 13:46:16 +0700

    new commit

--- src/projectgit/ProjectGit.java
+++ src/projectgit/ProjectGit.java
@@ -17,6 +17,7 @@ public class ProjectGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hallo");
+        System.out.println("Saya");
     }
     
 }
